1e8a34470f1420a90b35adcf290d0a131d2e184b
adamfisk <a@littleshoot.org>
2007-06-15 17:09:38 +0000

    STUN constants class
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/stun/stack@565 8d7aff80-242d-0410-bcee-ed6f15385b3d

---
+++ src/main/java/org/lastbamboo/common/stun/stack/StunConstants.java
@@ -0,0 +1,13 @@
+package org.lastbamboo.common.stun.stack;
+
+/**
+ * Constants for STUN, such as the default STUN port.
+ */
+public class StunConstants
+    {
+
+    /**
+     * The default port for STUN.
+     */
+    public static final int STUN_PORT = 3478;
+    }
