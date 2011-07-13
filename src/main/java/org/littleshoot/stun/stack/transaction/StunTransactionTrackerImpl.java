e2b58583384c1797b0f1052c016484bfebca2f32
Adam Fisk <a@littleshoot.org>
2011-07-12 22:03:45 -0700

    Formatting

--- src/main/java/org/littleshoot/stun/stack/transaction/StunTransactionTrackerImpl.java
+++ src/main/java/org/littleshoot/stun/stack/transaction/StunTransactionTrackerImpl.java
@@ -14,84 +14,79 @@ import org.littleshoot.stun.stack.message.StunMessage;
 /**
  * Class for matching requests and responses to their associated transactions.
  */
-public class StunTransactionTrackerImpl 
-    implements StunTransactionTracker<StunMessage>, StunTransactionListener
-    {
-    
-    private final Logger LOG = LoggerFactory.getLogger(StunTransactionTrackerImpl.class);
-    
-    private final Map<UUID, StunClientTransaction<StunMessage>> m_transactions = 
+public class StunTransactionTrackerImpl implements
+    StunTransactionTracker<StunMessage>, StunTransactionListener {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final Map<UUID, StunClientTransaction<StunMessage>> transactions = 
         new ConcurrentHashMap<UUID, StunClientTransaction<StunMessage>>();
-    
-    public void addTransaction(final StunMessage request, 
-        final StunTransactionListener listener, 
-        final InetSocketAddress localAddress, 
-        final InetSocketAddress remoteAddress)
-        {
+
+    @Override
+    public void addTransaction(final StunMessage request,
+            final StunTransactionListener listener,
+            final InetSocketAddress localAddress,
+            final InetSocketAddress remoteAddress) {
         final List<StunTransactionListener> transactionListeners = 
             new LinkedList<StunTransactionListener>();
         transactionListeners.add(listener);
-        
+
         final StunClientTransaction<StunMessage> ct = 
-            new StunClientTransactionImpl(request, transactionListeners,
+            new StunClientTransactionImpl(request, transactionListeners, 
                 remoteAddress);
         trackTransaction(ct);
-        }
+    }
 
-    private void trackTransaction(final StunClientTransaction<StunMessage> ct)
-        {
-        LOG.debug("Tracking transaction...");
+    private void trackTransaction(final StunClientTransaction<StunMessage> ct) {
+        log.debug("Tracking transaction...");
         final StunMessage message = ct.getRequest();
         final UUID key = getTransactionKey(message);
-        this.m_transactions.put(key, ct);
+        this.transactions.put(key, ct);
         ct.addListener(this);
-        }
+    }
 
+    @Override
     public StunClientTransaction<StunMessage> getClientTransaction(
-        final StunMessage message)
-        {
-        LOG.debug("Accessing client transaction...");
+            final StunMessage message) {
+        log.debug("Accessing client transaction...");
         final UUID key = getTransactionKey(message);
-        final StunClientTransaction<StunMessage> ct = 
-            this.m_transactions.get(key);
-        if (ct == null)
-            {
+        final StunClientTransaction<StunMessage> ct = this.transactions
+                .get(key);
+        if (ct == null) {
             // This will happen fairly often with STUN using UDP because
-            // multiple requests and responses could be sent.  We should just
+            // multiple requests and responses could be sent. We should just
             // silently ignore it.
-            LOG.debug("Nothing known about transaction: "+key);
-            LOG.debug("Known transactions: "+this.m_transactions.keySet());
-            }
+            log.debug("Nothing known about transaction: " + key);
+            log.debug("Known transactions: " + this.transactions.keySet());
+        }
         return ct;
-        }
+    }
 
-    private UUID getTransactionKey(final StunMessage message)
-        {
+    private UUID getTransactionKey(final StunMessage message) {
         return message.getTransactionId();
-        }
+    }
 
+    @Override
     public Object onTransactionFailed(final StunMessage request,
-        final StunMessage response)
-        {
-        LOG.debug("Transaction failed...");
+            final StunMessage response) {
+        log.debug("Transaction failed...");
         return removeTransaction(request);
-        }
+    }
 
-    public Object onTransactionSucceeded(final StunMessage request, 
-        final StunMessage response)
-        {
-        LOG.debug("Transaction succeeded...");
+    @Override
+    public Object onTransactionSucceeded(final StunMessage request,
+            final StunMessage response) {
+        log.debug("Transaction succeeded...");
         return removeTransaction(request);
-        }
+    }
 
-    private Object removeTransaction(final StunMessage message)
-        {
-        // We now consider the transaction completed and remove the 
+    private Object removeTransaction(final StunMessage message) {
+        // We now consider the transaction completed and remove the
         // transaction.
         final UUID key = getTransactionKey(message);
-        
-        LOG.debug("Removing transaction with key '" + key + "'");
-        this.m_transactions.remove(key);
+
+        log.debug("Removing transaction with key '" + key + "'");
+        this.transactions.remove(key);
         return null;
-        }
     }
+}
