93d5fffe484a352731d230437609cdbdbb9d2f86
Adam Fisk <a@littleshoot.org>
2012-04-11 18:39:52 -0700

    Switched to Google STUN servers by default

--- src/main/java/org/littleshoot/stun/stack/StunConstants.java
+++ src/main/java/org/littleshoot/stun/stack/StunConstants.java
@@ -15,14 +15,27 @@ public class StunConstants {
     public static final int STUN_PORT = 3478;
     
     public static InetSocketAddress[] SERVERS_ARRAY = {
-        new InetSocketAddress("stun.ideasip.com", StunConstants.STUN_PORT),
         //new InetSocketAddress("stun01.sipphone.com", StunConstants.STUN_PORT),
         //new InetSocketAddress("stun.softjoys.com", StunConstants.STUN_PORT),
-        new InetSocketAddress("stun.voipbuster.com", StunConstants.STUN_PORT),
-        new InetSocketAddress("stun.voxgratia.org", StunConstants.STUN_PORT),
-        new InetSocketAddress("stun.xten.com", StunConstants.STUN_PORT),
-        new InetSocketAddress("stun.sipgate.net", 10000),
-        new InetSocketAddress("numb.viagenie.ca", StunConstants.STUN_PORT) 
+        
+        //new InetSocketAddress("stun.ideasip.com", StunConstants.STUN_PORT),
+        
+        //new InetSocketAddress("stun.voipbuster.com", StunConstants.STUN_PORT),
+        //new InetSocketAddress("stun.voxgratia.org", StunConstants.STUN_PORT),
+        //new InetSocketAddress("stun.xten.com", StunConstants.STUN_PORT),
+        //new InetSocketAddress("stun.sipgate.net", 10000),
+        //new InetSocketAddress("numb.viagenie.ca", StunConstants.STUN_PORT)
+
+        new InetSocketAddress("stun.l.google.com", 19302), 
+        new InetSocketAddress("stun.l.google.com", 19302), 
+        new InetSocketAddress("alt4.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt4.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt3.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt3.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt1.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt1.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt2.stun.l.google.com", 19302), 
+        new InetSocketAddress("alt2.stun.l.google.com", 19302)
     };
     
     public static List<InetSocketAddress> SERVERS = Arrays.asList(SERVERS_ARRAY);
